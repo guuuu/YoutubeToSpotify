2d81a54986c25be5787997699ce62a6894b8d6cf
Gustavo Nascimento <guganascimento2000@gmail.com>
2020-12-21 04:29:06 +0000

    Server

---
+++ Site/assets/js/script.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 8080
+const path = require("path");
+const fs = require("fs");
+const html_path = path.join(__dirname, "..\\..");
+const default_folder = path.join(__dirname, "..\\..\\");
+
+app.use(express.static(default_folder))
+
+app.use('/:page', function(req, res) {
+    let req_file = path.join(html_path, req.originalUrl + ".html");
+
+    try {
+        fs.access(req_file, fs.F_OK, (err) => {
+            if (err){
+                res.status(404);
+                res.sendFile(default_file);
+            }
+            else{
+                res.status(200);
+                res.sendFile(req_file);
+            }
+        });    
+    } catch (error) {
+        res.status(404);
+        res.sendFile(req_file);
+    }
+});
+
+app.listen(port, console.log(`Server listening in port ${port}`));
